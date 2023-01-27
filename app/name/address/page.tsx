2ded9a0e8204a8e9dd4cac12548109cebbe4245c
devera-fx <aakifnajib@gmail.com>
2023-01-27 21:04:21 +0500

    completed linking

---
+++ app/name/address/page.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+
+export default function GiveAddress() {
+    return (
+        <div>
+            I live in Lahore, Pakistan
+            <br/>
+          <Link href="/">Go to Home page</Link>
+        </div>
+    )
+}
